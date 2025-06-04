3ba749fd6404e03d4c26703b5db648f9c199b6f2
Gorantla Mokshgna <gorantlamokshgna980@gmail.com>
2025-06-04 09:11:08 +0530

    fixed some bugs in user routes

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -8,61 +8,61 @@ router.use(protectRoute);
 
 
 //
-router.post("/users/addCourse",instructorAccess,addCourse);
+router.post("/addCourse",instructorAccess,addCourse);
 
 
 
-router.post('/users/addLesson',instructorAccess,addLesson);
+router.post('/addLesson',instructorAccess,addLesson);
 
 
 
-router.post('/users/getAllCourses',getAllCourses);
+router.post('/getAllCourses',getAllCourses);
 
 
 
-router.post('/users/enrollInCourse',enroll);//we will have to add the payment middle ware here like this router.post('/',payment,enroll);
+router.post('/enrollInCourse',enroll);//we will have to add the payment middle ware here like this router.post('/',payment,enroll);
 
 
 
-router.post('/users/getCurrentCourse',getCurrentCourse);
+router.post('/getCurrentCourse',getCurrentCourse);
 
 
 
-router.post('/users/getDiscussion',getDiscussion);
+router.post('/getDiscussion',getDiscussion);
 
 
 
-router.post('/users/addMessage',addMessage);
+router.post('/addMessage',addMessage);
 
 
 
-router.post('/users/getPendingRequests',adminaccess,getPendingRequests);
+router.post('/getPendingRequests',adminaccess,getPendingRequests);
 
 
 
-router.post('/users/getLessons',enrollmentAccess,getLesson);// we will need the index of the lesson to the post index . 
+router.post('/getLessons',enrollmentAccess,getLesson);// we will need the index of the lesson to the post index . 
 
 
 
-router.post('/users/updateProgress',enrollmentAccess,updateProgress);
+router.post('/updateProgress',enrollmentAccess,updateProgress);
 
 
 
-router.post('/users/getProgress',getProgress);
+router.post('/getProgress',getProgress);
 
-router.post('/users/addQuiz',instructorAccess,addQuiz);
+router.post('/addQuiz',instructorAccess,addQuiz);
 
-router.post('/users/submitQuiz',submitQuiz);
+router.post('/submitQuiz',submitQuiz);
 
-router.post('/users/addToCart',addToCart);
+router.post('/addToCart',addToCart);
 
-router.post('/users/removeFromCart',removeFromCart);
+router.post('/removeFromCart',removeFromCart);
 
-router.post('/users/getCartCourses',getCartCourses);
+router.post('/getCartCourses',getCartCourses);
 
-router.post('/users/getQuiz',getQuiz);
+router.post('/getQuiz',getQuiz);
 
-router.post('/users/getMyCourses',getMyCourses);
+router.post('/getMyCourses',getMyCourses);
 
 
 
