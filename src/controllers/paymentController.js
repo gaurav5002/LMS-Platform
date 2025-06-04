c480d0d8d9f893f5264ec14909c1e166a1644ec3
Gorantla Mokshgna <gorantlamokshgna980@gmail.com>
2025-06-04 21:56:12 +0530

    initial commit

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -3,7 +3,7 @@ import crypto from "crypto";
 import Payment from "../models/payment.js"
 import User from "../models/user.js"
 import { Course } from "../models/Course.js";
-
+import Cart from "../models/Cart.js";
 import dotenv from 'dotenv'
 dotenv.config();
 
@@ -76,6 +76,20 @@ export const verifyPaymentAndEnroll = async (req, res) => {
     if(!user){
       return res.status(404).json({message:"user not found ",success:false});
     }
+    const cart = await Cart.findOne({userId:user._id});
+            if(!cart){
+                 return res.status(404).json({success:false,message:"cart inexistent this message should never occur ..."});
+            }
+            const cartCourses = cart.courses;
+            if(user.enrolledCourses.includes(courseId)){
+                return res.status(201).json({success:false,message:"cannot add enrolled courses into cart! "})
+            }
+            if(!cartCourses.includes(courseId)){
+                cartCourses.push(courseId);
+            }
+            await Cart.findByIdAndUpdate(cart._id,{
+                courses:cartCourses
+            });
     
     const enrolledCourses = user.enrolledCourses;
     await User.findByIdAndUpdate(userId,{
