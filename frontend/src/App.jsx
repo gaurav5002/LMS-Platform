e2363a300c9dd9252800fc6763a1659b156c2cf5
Gourav Sarraf <153271011+GouravSarraf@users.noreply.github.com>
2025-06-10 16:13:44 +0530

    fixed error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -119,7 +119,7 @@ function App() {
             path="/register"
             element={
               <PublicRoute>
-                <RegisterPage />
+                <RegisterPage onRegister={initialize} />
               </PublicRoute>
             }
           />
